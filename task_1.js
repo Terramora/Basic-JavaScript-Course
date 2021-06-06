4e84c559b6efb27113840abd7546ef41b053f6da
Terramora <40336791+Terramora@users.noreply.github.com>
2021-06-06 12:17:38 +0300

    Update task_1.js

--- task_1.js
+++ task_1.js
@@ -5,21 +5,25 @@
 
 
 function f(num) {
+    if (num < 0 || num > 999) {
+        console.log('Число не соответствует диапазону!');
+        return {};
+    }
     let result = {
         units: 0,
         dozens: 0,
         hundreds: 0
     };
     for (let i = 0; num > 0; i++) {
-        let key = ''
-        if (i === 0) key = 'units'
-        else if (i === 1) key = 'dozens'
-        else key = 'hundreds'
+        let key = '';
+        if (i === 0) key = 'units';
+        else if (i === 1) key = 'dozens';
+        else key = 'hundreds';
         result[key] = num % 10;
-        num = parseInt(num / 10)
+        num = parseInt(num / 10);
 
     }
-    return result
+    return result;
 }
 
-console.log(f(987))
+console.log(f(1000))
